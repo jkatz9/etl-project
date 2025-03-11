95fda1501ab0e4bd8939fd55b689aa229621bd16
Justin Katz <jukatz93@gmail.com>
2025-03-11 11:31:22 -0400

    sqlstuff

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,49 +1,44 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/7lWmKy
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+create database "crowdfunding_db"
 
+DROP TABLE IF EXISTS "campaign";
+DROP TABLE IF EXISTS "contacts";
+DROP TABLE IF EXISTS "subcategory";
+DROP TABLE IF EXISTS "category";
 
 CREATE TABLE "category" (
-    "Category_id" varchar   NOT NULL,
-    "Category_name" varchar   NOT NULL,
-    CONSTRAINT "pk_category" PRIMARY KEY (
-        "Category_id"
-     )
+    "Category_id" varchar NOT NULL PRIMARY KEY,
+    "Category_name" varchar NOT NULL
 );
 
-CREATE TABLE "campaign" (
-    "Cf_id" int   NOT NULL,
-    "Contact_id" int   NOT NULL,
-    "Company_name" varchar   NOT NULL,
-    "Description" varchar   NOT NULL,
-    "Goal" int   NOT NULL,
-    "Pleged" int   NOT NULL,
-    "Outcome" varchar   NOT NULL,
-    "Backers_count" int   NOT NULL,
-    "Country" varchar   NOT NULL,
-    "Currency" varchar   NOT NULL,
-    "Launch_date" date   NOT NULL,
-    "End_date" date   NOT NULL,
-    "Category_id" varchar   NOT NULL,
-    "Subcategory_id" varchar   NOT NULL,
-    CONSTRAINT "pk_campaign" PRIMARY KEY (
-        "Cf_id"
-     )
+CREATE TABLE "subcategory" (
+    "Subcategory_id" varchar NOT NULL PRIMARY KEY,
+    "Subcategory_name" varchar NOT NULL
 );
 
 CREATE TABLE "contacts" (
-    "Contact_id" int   NOT NULL,
-    "First_name" varchar   NOT NULL,
-    "Last_name" varchar   NOT NULL,
-    "email" varchar   NOT NULL,
-    CONSTRAINT "pk_contacts" PRIMARY KEY (
-        "Contact_id"
-     )
+    "Contact_id" int NOT NULL PRIMARY KEY,
+    "First_name" varchar NOT NULL,
+    "Last_name" varchar NOT NULL,
+    "email" varchar NOT NULL
 );
 
-ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_Contact_id" FOREIGN KEY("Contact_id")
-REFERENCES "contacts" ("Contact_id");
-
-ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_Category_id" FOREIGN KEY("Category_id")
-REFERENCES "category" ("Category_id");
+CREATE TABLE "campaign" (
+    "Cf_id" int NOT NULL PRIMARY KEY,
+    "Contact_id" int NOT NULL,
+    "Company_name" varchar NOT NULL,
+    "Description" varchar NOT NULL,
+    "Goal" int NOT NULL,
+    "Pledged" int NOT NULL,
+    "Outcome" varchar NOT NULL,
+    "Backers_count" int NOT NULL,
+    "Country" varchar NOT NULL,
+    "Currency" varchar NOT NULL,
+    "Launch_date" date NOT NULL,
+    "End_date" date NOT NULL,
+    "Category_id" varchar NOT NULL,
+    "Subcategory_id" varchar NOT NULL,
+    FOREIGN KEY ("Contact_id") REFERENCES "contacts" ("Contact_id"),
+    FOREIGN KEY ("Category_id") REFERENCES "category" ("Category_id"),
+    FOREIGN KEY ("Subcategory_id") REFERENCES "subcategory" ("Subcategory_id")
+);
 
